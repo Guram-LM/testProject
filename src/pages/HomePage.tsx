6ee7c5ad3762b9b4e95e1435188e5f661fce2e2b
Nino Khurtsidze <103818368+Ninunutsi@users.noreply.github.com>
2025-06-11 21:24:36 +0400

    fix: search functionality

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,37 +1,40 @@
-import { useState, useTransition } from "react"
-import { useLoaderData } from "react-router-dom"
+import { useState, useTransition } from "react";
+import { useLoaderData } from "react-router-dom";
 
 export const HomePage = () => {
+  const cantryData = useLoaderData();
+  const [pending, startTransition] = useTransition();
+  const [value, setValue] = useState("");
+  const [countries, setCountries] = useState(cantryData);
 
-    const cantryData = useLoaderData()
-    const [pending, startTransition] = useTransition()
-    const [value, setValue] = useState('')
-    const [countries, setCountries] = useState(cantryData)
-
-
-
-
-    const onChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
-        setValue(e.target.value)
-        fetch(`https://restcountries.com/v3.1/name/${e.target.value}?fullText=true`)
-        .then((res)=>(res.json()))
-        .then((newResponse)=>(startTransition(()=>(setCountries(newResponse)))))
-        .catch(()=>startTransition(()=>setCountries([])))
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+    if (e.target.value.trim() === "") {
+      setCountries(cantryData);
+      return;
     }
 
-    console.log(countries)
+    fetch(`https://restcountries.com/v3.1/name/${e.target.value}`)
+      .then((res) => res.json())
+      .then((newResponse) => startTransition(() => setCountries(newResponse)))
+      .catch(() => startTransition(() => setCountries([])));
+  };
 
-    return(
-        <div>
-            <input type="text" onChange={onChange} value={value}/>
-            {pending && <p>Loading . . .</p>}
-            {countries.map((cantry: any) => 
-                (<div key={cantry.cca2}>
-                    <h2>{cantry.name.common} </h2>
-                </div>)
-            )}
-        </div>
+  console.log(countries);
 
-        
-    )
-}
+  return (
+    <div>
+      <input type="text" onChange={onChange} value={value} />
+      {pending && <p>Loading . . .</p>}
+      {!Array.isArray(countries) ? (
+        <p>please enter a name</p>
+      ) : (
+        countries?.map((cantry: any) => (
+          <div key={cantry.cca2}>
+            <h2>{cantry.name.common} </h2>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
